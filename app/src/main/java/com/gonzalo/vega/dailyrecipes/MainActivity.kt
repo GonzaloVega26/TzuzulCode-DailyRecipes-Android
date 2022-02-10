d937fa13348d82d1cd82f5405e83451beeed4713
Gonzalo Vega <31gonzalovega@gmail.com>
2022-02-10 11:13:12 -0300

    Initial commit

---
+++ app/src/main/java/com/gonzalo/vega/dailyrecipes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gonzalo.vega.dailyrecipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
